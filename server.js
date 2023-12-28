5cdf1afdb1c7f9f42cb884a18ca062b26520638f
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-12-28 23:25:35 +0530

    update

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require("express");
-const StreamAudio = require("youtube-dl-exec");
+const StreamAudio = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
+const fs = require("fs");
 
 app.use(cors());
+app.use(express.static('./'))
 
 app.get("/", async (req, res) => {
-  const link = req.query.url;
-  if (link) {
-    StreamAudio(link, {
-      dumpSingleJson: true,
-      noCheckCertificates: true,
-      noWarnings: true,
-      preferFreeFormats: true,
-      format:"best",
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    })
-      .then((output) => {
-        return output;
-      })
-      .then((data) => {
-        res.send(data.requested_downloads[0].url);
-      }).catch(err=>{
-        console.log(err);
-      })
-  } else {
-    res.json({ message: "url not provided" });
-  }
+    try {
+        const link = req.query.url;
+        if (link) {
+          const id = StreamAudio.getVideoID(link);
+         if(fs.existsSync(`./music/${id}.mp4`)){
+             res.send(`/music/${id}.mp4`)
+             return
+         } 
+          const url = StreamAudio(link,{filter:'videoandaudio',quality:'highestvideo'}).pipe(fs.createWriteStream(`./music/${id}.mp4`))
+          url.on('finish',()=>{
+              res.send(`/music/${id}.mp4`);
+          })
+        } else {
+          res.json({ message: "url not provided" });
+        }
+    } catch (error) {
+        res.json({error: error.message});
+    }
+
 });
 
 app.listen(port, () => {
